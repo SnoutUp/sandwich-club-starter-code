fd6cc7997b556807d410166883a02e7a47f63d52
Aurimas <agaruolis@lrytas.lt>
2018-02-16 12:48:37 +0200

    Display sandwich information in DetailActivity

--- app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
+++ app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
@@ -1,12 +1,17 @@
 package com.udacity.sandwichclub;
 
 import android.content.Intent;
+import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
+import android.view.MenuItem;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.squareup.picasso.Picasso;
+import com.udacity.sandwichclub.databinding.ActivityDetailBinding;
 import com.udacity.sandwichclub.model.Sandwich;
 import com.udacity.sandwichclub.utils.JsonUtils;
 
@@ -15,12 +20,16 @@ public class DetailActivity extends AppCompatActivity {
     public static final String EXTRA_POSITION = "extra_position";
     private static final int DEFAULT_POSITION = -1;
 
+    ActivityDetailBinding mBinding;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_detail);
 
-        ImageView ingredientsIv = findViewById(R.id.image_iv);
+        mBinding = DataBindingUtil.setContentView(this, R.layout.activity_detail);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        ImageView ingredientsIv = findViewById(R.id.iv_image);
 
         Intent intent = getIntent();
         if (intent == null) {
@@ -43,7 +52,7 @@ public class DetailActivity extends AppCompatActivity {
             return;
         }
 
-        populateUI();
+        populateUI(sandwich);
         Picasso.with(this)
                 .load(sandwich.getImage())
                 .into(ingredientsIv);
@@ -56,7 +65,33 @@ public class DetailActivity extends AppCompatActivity {
         Toast.makeText(this, R.string.detail_error_message, Toast.LENGTH_SHORT).show();
     }
 
-    private void populateUI() {
+    private void populateUI(Sandwich sandwich) {
+        if (sandwich.getPlaceOfOrigin().length() > 0) {
+            mBinding.tvOrigin.setText(sandwich.getPlaceOfOrigin());
+        } else {
+            mBinding.tvOrigin.setVisibility(View.GONE);
+            mBinding.tvOriginLabel.setVisibility(View.GONE);
+        }
 
+        if (sandwich.getAlsoKnownAs().size() > 0) {
+            mBinding.tvAlsoKnown.setText(TextUtils.join(", ", sandwich.getAlsoKnownAs()));
+        } else {
+            mBinding.tvAlsoKnown.setVisibility(View.GONE);
+            mBinding.tvAlsoKnownLabel.setVisibility(View.GONE);
+        }
+
+
+        mBinding.tvDescription.setText(sandwich.getDescription());
+        mBinding.tvIngredients.setText(TextUtils.join(", ", sandwich.getIngredients()));
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                finish();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
--- app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
+++ app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
@@ -2,9 +2,62 @@ package com.udacity.sandwichclub.utils;
 
 import com.udacity.sandwichclub.model.Sandwich;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class JsonUtils {
 
+    /*
+    {\"name\":{\"mainName\":\"Ham and cheese
+            sandwich\",\"alsoKnownAs\":[]},
+
+            \"placeOfOrigin\":\"\",
+            \"description\":\"A ham and cheese
+            sandwich is a common type of sandwich. It is made by putting cheese and sliced ham
+            between two slices of bread. The bread is sometimes buttered and/or toasted. Vegetables
+            like lettuce, tomato, onion or pickle slices can also be included. Various kinds of
+            mustard and mayonnaise are also
+            common.\",
+            \"image\":\"https://upload.wikimedia.org/wikipedia/commons/thumb/5/50/Grilled_ham_and_cheese_014.JPG/800px-Grilled_ham_and_cheese_014.JPG\",
+            \"ingredients\":[\"Sliced bread\",\"Cheese\",\"Ham\"]}
+     */
+
     public static Sandwich parseSandwichJson(String json) {
+        try {
+            JSONObject jo = new JSONObject(json);
+            JSONObject names = jo.getJSONObject("name");
+            String mainName = names.getString("mainName");
+
+
+            JSONArray nameArray = names.getJSONArray("alsoKnownAs");
+            int nameCount = nameArray.length();
+            List<String> alsoKnownAs = new ArrayList<>();
+            for (int i = 0; i < nameCount; i++) {
+                alsoKnownAs.add(nameArray.getString(i));
+            }
+
+
+            JSONArray ingredientArray = jo.getJSONArray("ingredients");
+            int ingredientCount = ingredientArray.length();
+            List<String> ingredients = new ArrayList<>();
+            for (int i = 0; i <ingredientCount; i++) {
+                ingredients.add(ingredientArray.getString(i));
+            }
+
+            String origin = jo.getString("placeOfOrigin");
+            String description = jo.getString("description");
+            String image = jo.getString("image");
+
+
+            Sandwich parsedSandwich = new Sandwich(mainName, alsoKnownAs, origin, description, image, ingredients);
+            return parsedSandwich;
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
         return null;
     }
 }
